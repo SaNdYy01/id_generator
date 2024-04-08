888b13e1df4b976c7aaa3cce6fc3f089a7b1d2d2
SaNdYy01 <105999799+SaNdYy01@users.noreply.github.com>
2024-04-08 23:45:30 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,13 @@
+
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "ccbd_id_card_genarator";
+
+// Create a connection
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+// Die if connection was not successful
+?>
+
